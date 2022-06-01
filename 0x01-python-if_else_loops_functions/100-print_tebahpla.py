f500e0cc636dcf38813ab510aea0722df2c46ef5
Tebogo.ramoretli1998@gmail.com <Mabontle@20>
2022-06-01 05:32:21 +0200

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if (i % 2 == 0):
-        print("{}".format(chr(i)), end="")
-    else:
-        print("{}".format(chr(i - 32)), end="")
+for ascii_num in range(122, 96, -1):
+	if ascii_num % 2 == 1:
+		ascii_num = ascii_num - 32
+	print("{:c}".format(ascii_num), end='')
