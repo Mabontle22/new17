c1476abdccc3c96342776a6b5b099382b932f25b
Tebogo.ramoretli1998@gmail.com <Mabontle@20>
2022-06-01 05:39:05 +0200

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-for x in range(10):
-    for y in range(10):
-        if x < y:
-            if (x * 10 + y) != 89:
-                print("{}{},".format(x, y), end=" ")
-            else:
-                print("{}{}".format(x, y))
+
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i >= j:
+            continue
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
